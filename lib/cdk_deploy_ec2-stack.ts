b2bcd4cc83c48fb2c8326ab001facb0ab5803ca0
EmiRoberti77 <emi@datamotion.ai>
2024-12-09 05:52:55 +0000

    added elastic ip and open custom port for inbound traffic on the subnet security group

--- lib/cdk_deploy_ec2-stack.ts
+++ lib/cdk_deploy_ec2-stack.ts
@@ -11,7 +11,7 @@ export class CdkDeployEc2Stack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, process.env.VPC_NAME!, {
       cidr: process.env.CIDR,
-      maxAzs: 1, // Only one AZ for simplicity
+      maxAzs: 1,
       subnetConfiguration: [
         {
           cidrMask: 24,
@@ -23,51 +23,72 @@ export class CdkDeployEc2Stack extends cdk.Stack {
 
     const keyName = process.env.KEY_NAME!;
 
-    // Security Group for EC2 with SSH access
     const securityGroup = new ec2.SecurityGroup(
       this,
       process.env.SECURITY_GROUP!,
       {
         vpc,
-        allowAllOutbound: true, // Allow outbound traffic
-        description: "Allow SSH access",
+        allowAllOutbound: true,
+        description: "HTTP, HTTPS, SSH, and application ports access",
       }
     );
+
     securityGroup.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(22),
       "Allow SSH access"
     );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "Allow HTTP access"
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      "Allow HTTPS access"
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(8000),
+      "custom port for node service"
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(8080),
+      "custom port for node servive"
+    );
 
-    // IAM Role for EC2
     const role = new iam.Role(this, process.env.EC2_INSTANCE_ROLE!, {
       assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
     });
 
-    // Add permissions to the role (optional, for SSM or other services)
     role.addManagedPolicy(
       iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
     );
 
-    // Launch an EC2 instance
     const instance = new ec2.Instance(this, process.env.EC2_INSTANCE!, {
       vpc,
       instanceType: ec2.InstanceType.of(
         ec2.InstanceClass.T2,
         ec2.InstanceSize.MICRO
       ),
-      machineImage: ec2.MachineImage.latestAmazonLinux(), // Amazon Linux 2 AMI
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
       securityGroup,
-      keyName, // Specify the key pair
+      keyName,
       role,
       vpcSubnets: {
         subnetType: ec2.SubnetType.PUBLIC,
       },
     });
 
+    const elasticIp = new ec2.CfnEIP(this, "ElasticIp", {
+      instanceId: instance.instanceId,
+    });
+
     new cdk.CfnOutput(this, "InstancePublicIP", {
-      value: instance.instancePublicIp,
-      description: "Public IP of the EC2 instance",
+      value: elasticIp.ref,
+      description: "Elastic IP of the EC2 instance",
     });
   }
 }
